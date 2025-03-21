2589fb9eac1e108f88460c7a9f32ba4794de8321
SaiKrishna <tullurisaikrishna1996@gmail.com>
2025-03-21 13:08:03 +0530

    1

--- ServerDetails/express.js
+++ ServerDetails/express.js
@@ -174,7 +174,7 @@ app.post('/login', async (request, response) => {
                 }else{
                     
                     response.status(400);
-                    response.send(err);            
+                    response.send({err_msg: err});            
                 
             };
             
